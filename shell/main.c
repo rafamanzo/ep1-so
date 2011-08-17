67d8f3fb4e6883e7d11899f70f6ec2c4d8bc6072
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2011-08-17 07:29:43 -0300

    Listando diretório corrente

--- shell/main.c
+++ shell/main.c
@@ -6,11 +6,20 @@
  */
 
 #include <ncurses.h>
+#include <dirent.h>
 
 int main()
 {
+	DIR *dp;
+	struct dirent *entry;
+
 	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
+
+	dp = opendir(".");
+	while ((entry = readdir(dp))){
+		printw("\n %s", entry->d_name);
+	}
+
 	refresh();			/* Print it on to the real screen */
 	getch();			/* Wait for user input */
 	endwin();			/* End curses mode		  */
